492b30b16934d9113d6f87522988e0aa2e18bf12
Nicholas Parson <Nparson88@gmail.com>
2021-06-29 15:02:49 -0400

    attempted manifest fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,11 @@
 const FILES_TO_CACHE = [
     '/',
-    '/index.html',
-    '/index.js',
-    '/styles.css',
-    '/manifest.json',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
+    '/public/index.html',
+    '/public/index.js',
+    '/public/styles.css',
+    '/public/manifest.webmanifest',
+    '/public/icons/icon-192x192.png',
+    '/public/icons/icon-512x512.png',
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
     "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ]
