7c237055c99683fd05534312210e14a9d8839e45
Nicholas Parson <Nparson88@gmail.com>
2021-06-29 21:25:14 -0400

    fixes and comments

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-
+// heroku port
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
+//atlas db port
 mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/budget", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
